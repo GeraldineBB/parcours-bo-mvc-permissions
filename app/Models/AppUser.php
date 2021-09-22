d05464bb6836fcfd85b6beb0cbb9f05260f74bd7
GeraldineBB <geraldine-bornet@hotmail.fr>
2021-09-22 18:15:25 +0200

    appuser en cours

--- app/Models/AppUser.php
+++ app/Models/AppUser.php
@@ -8,7 +8,99 @@ use PDO;
 class AppUser extends CoreModel
 {
 
-  // ***********************************
-  // Propriétés
-  // ***********************************
+    // PROPRIETES 
+
+
+
+
+    // METHODES 
+
+
+    // -----------------------------------------------
+    // [C]RUD
+    // -----------------------------------------------
+
+    /**
+     * Insérer un nouveau prof dans la BDD
+     * 
+     */
+    public function insert()
+    {
+        // se connecter à la BDD
+        $pdo = Database::getPDO();  
+        
+        // écriture de la requête 
+        $sql = "
+        INSERT INTO `teacher` (firstname, lastname, job, status)
+        VALUES (:firstname, :lastname, :job, :status)
+        ";
+
+        // préparation de la requête 
+        $preparation = $pdo->prepare($sql);
+
+        // execution de la requête
+        $inserted = $preparation->execute([
+            ":firstname" => $this->firstname,
+            ":lastname" => $this->lastname,
+            ":job" => $this->job, 
+            ":status" => $this->status, 
+        ]);
+
+        if($inserted){
+            $this->id = $pdo->lastInsertId();
+            return true;
+
+        } else {
+            return false; 
+        }
+    
+    }
+
+    // -----------------------------------------------
+    // C[R]UD
+    // -----------------------------------------------
+
+    static public function find($id)
+    {
+        // pas besoin 
+    }
+
+    /**
+     * Récupérer la liste des profs en BDD
+     * 
+     */
+    static public function findAll()
+    {
+        $pdo = Database::getPDO();
+        $sql = 'SELECT * FROM `teacher`';
+        $pdoStatement = $pdo->query($sql);
+        $results = $pdoStatement->fetchAll(PDO::FETCH_CLASS, 'App\Models\Teacher');
+        
+        return $results;
+
+    }
+
+    // -----------------------------------------------
+    // CR[U]D
+    // -----------------------------------------------
+
+    public function update()
+    {
+        // pas besoin 
+    }
+
+    // -----------------------------------------------
+    // CRU[D]
+    // -----------------------------------------------
+
+    public function delete()
+    {
+        // pas besoin
+    }
+
+    // GETTER AND SETTER 
+
+
+
+    
 }
